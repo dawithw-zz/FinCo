147872e371344b6defb2dc94656c9da8426fa50a
Ali Asad <Ali Asad@172.19.144.34>
2018-10-24 00:42:48 +0500

    Changed AccountFactory from interface to class. Created simple CustomerAccountFactory as a mediator between app and factories

--- src/controller/AccountFactory.java
+++ src/controller/AccountFactory.java
@@ -3,7 +3,8 @@ package controller;
 import model.Account;
 import model.Customer;
 
-public interface AccountFactory {
-	public Customer createCustomer(String name, String email, String street, String city, String state, String zip);
-	public Account createAccount(Customer owner,  long accountNumber);
+public class AccountFactory {
+	public Account createAccount(Customer owner,  long accountNumber) {
+		return new Account(owner, accountNumber);
+	}
 }
