a96f2fe78d5d13e29bb0a45d4bf3fe82dd8fdedf
Dawit Woldegiorgis <dwoldegiorgis@mum.edu>
2018-10-23 18:32:38 -0500

    make AccountFactory constructor private

--- src/controller/AccountFactory.java
+++ src/controller/AccountFactory.java
@@ -5,9 +5,15 @@ import model.Customer;
 
 public class AccountFactory {
 	private static AccountFactory instance = new AccountFactory();
+	
+	private AccountFactory() {
+		// should not be invoked except by instance
+	}
+	
 	public Account createAccount(Customer owner,  String accountNumber) {
 		return new Account(owner, accountNumber);
 	}
+	
 	public static AccountFactory getInstance() {
 		return instance;
 	}
