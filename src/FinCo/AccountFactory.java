49efe03f38ef1c89f1f28704d78766d11380283e
Dawit Woldegiorgis <dwoldegiorgis@mum.edu>
2018-10-22 21:34:57 -0500

    add abstract account

---
+++ src/FinCo/AccountFactory.java
@@ -0,0 +1,6 @@
+package FinCo;
+
+public interface AccountFactory {
+	public Customer createCustomer(String name, String email, String street, String city, String state, String zip);
+	public Account createAccount(Customer owner);
+}
