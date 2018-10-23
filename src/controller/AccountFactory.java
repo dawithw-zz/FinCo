1433a386ce5203399b4e5a46d00df0910c610478
Ali Asad <Ali Asad@172.19.144.34>
2018-10-23 08:07:25 +0500

    Moved AccountFactory to controller

--- src/controller/AccountFactory.java
+++ src/controller/AccountFactory.java
@@ -1,4 +1,7 @@
-package model;
+package controller;
+
+import model.Account;
+import model.Customer;
 
 public interface AccountFactory {
 	public Customer createCustomer(String name, String email, String street, String city, String state, String zip);
