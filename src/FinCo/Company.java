89cb9d49aa73870e2826daec3ffba3162465e616
Ali Asad <Ali Asad@172.19.144.34>
2018-10-23 07:55:06 +0500

    updated company

--- src/FinCo/Company.java
+++ src/FinCo/Company.java
@@ -1,11 +1,12 @@
 package FinCo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Company extends Customer implements ICompany {
 
 	//private int noOfEmployees;
-	private List<Person> persons;
+	private List<Person> persons = new ArrayList<>();
 	
 	public Company(String name, String email, Address address) {
 		super(name, email, address);
