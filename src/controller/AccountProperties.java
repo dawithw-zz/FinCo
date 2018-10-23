678047eba98040c5e8b9217364b577f5c4a18333
Dawit Woldegiorgis <dwoldegiorgis@mum.edu>
2018-10-22 23:05:36 -0500

    added AccountProperties, added new methods to FinCo

---
+++ src/controller/AccountProperties.java
@@ -0,0 +1,68 @@
+package controller;
+
+public class AccountProperties {
+	private String accountNumber;
+	private String clientName;
+	private String streetName;
+	private String city;
+	private String state;
+	private String zip;
+	private String accountType;
+	private String amountDeposit;
+	
+	public AccountProperties() {
+		accountNumber = clientName = streetName = city = state = zip = accountType = amountDeposit = "";
+	}
+	
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public String getClientName() {
+		return clientName;
+	}
+	public void setClientName(String clientName) {
+		this.clientName = clientName;
+	}
+	public String getStreetName() {
+		return streetName;
+	}
+	public void setStreetName(String streetName) {
+		this.streetName = streetName;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getZip() {
+		return zip;
+	}
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+	public String getAccountType() {
+		return accountType;
+	}
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
+	public String getAmountDeposit() {
+		return amountDeposit;
+	}
+	public void setAmountDeposit(String amountDeposit) {
+		this.amountDeposit = amountDeposit;
+	}
+
+	
+	
+}
--- src/controller/FinCo.java
+++ src/controller/FinCo.java
@@ -1,11 +1,46 @@
 package controller;
 
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import model.Account;
 import model.Customer;
+import model.Transaction;
 
 public class FinCo {
-	Map<Customer, Account> accounts = new HashMap<>();
+	Map<Customer, List<Account>> accounts = new HashMap<>();
+	
+	public void addPersonalAccount(AccountProperties param) {
+		
+	}
+	
+	public void addCompanyAccount(AccountProperties param) {
+		
+	}
+	
+	public void addInterest() {
+		for(Customer c : accounts.keySet()) {
+			accounts.get(c).stream().forEach(account -> account.addInterest());
+		}
+	}
+	
+	public void deposit(String customerName, String accountNumber, String deposit) {
+		Customer customer = accounts.keySet().stream()
+				.filter(c -> c.getName().equals(customerName))
+				.findFirst().get();
+		
+		accounts.get(customer).stream()
+			.filter(account -> account.accountNumber() == Long.parseLong(accountNumber))
+			.findFirst().get()
+			.addTransaction(new Transaction("Deposit", Double.parseDouble(deposit), Date.valueOf(LocalDate.now())));
+	}
+	
+	public void withdraw(String customerName, String accountNumber, String withdrawal) {
+		
+	}
+	
+	
 }
