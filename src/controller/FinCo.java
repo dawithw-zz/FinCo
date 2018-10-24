b3efc87affc80ad55ee347d95201fff3bbcfebbb
Dawit Woldegiorgis <dwoldegiorgis@mum.edu>
2018-10-23 20:55:57 -0500

    implemented FinCo methods

--- src/controller/FinCo.java
+++ src/controller/FinCo.java
@@ -3,23 +3,19 @@ package controller;
 public class FinCo {
 	
 	public void addPersonalAccount(AccountParameters param) {
-	
+		CustomerAccountFactory.createPersonalAccount(param);
 	}
 	
 	public void addCompanyAccount(AccountParameters param) {
-		
-	}
-	
-	public void addInterest() {
-		
+		CustomerAccountFactory.createCompanyAccount(param);
 	}
 	
 	public void deposit(String customerName, String accountNumber, String deposit) {
-		
+		CustomerAccountFactory.deposit(accountNumber, Double.parseDouble(deposit));
 	}
 	
 	public void withdraw(String customerName, String accountNumber, String withdrawal) {
-		
+		CustomerAccountFactory.withdraw(accountNumber, Double.parseDouble(withdrawal));
 	}
 	
 }
