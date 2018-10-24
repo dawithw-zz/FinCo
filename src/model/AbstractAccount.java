f0ec956187742aea4a56b38f07674e015462a0cf
Dawit Woldegiorgis <dwoldegiorgis@mum.edu>
2018-10-24 18:06:13 -0500

    refactored Account parameters and methods into AbstractAccount

--- src/model/AbstractAccount.java
+++ src/model/AbstractAccount.java
@@ -1,9 +1,15 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class AbstractAccount implements IAccount {
 	
 	private ICustomer owner;
 	protected double interest;
+	protected List<ITransaction> transactions = new ArrayList<>();
+	protected String accountNumber;
+	protected double currentBalance;
 	
 	// add observer
 	@Override
@@ -25,6 +31,33 @@ public abstract class AbstractAccount implements IAccount {
 	@Override
 	public void setInterest(double interest) {
 		this.interest = interest;
+	}
+	
+	@Override
+	public String accountNumber() {
+		return accountNumber;
+	}
+	
+	@Override
+	public void addTransaction(ITransaction transaction) {
+		transactions.add(transaction);
+		currentBalance += transaction.amount();
+	}
+
+	@Override
+	public double getBalance() {
+		return currentBalance;
+	}
+
+	@Override
+	public String toString() {
+		return "Account = accountNumber: " + accountNumber + ", currentBalance: " + currentBalance;
+	}
+
+	@Override
+	public void addInterest() {
+	 this.currentBalance += this.currentBalance * this.getInterest();
+		
 	}
 
 	
--- src/model/Account.java
+++ src/model/Account.java
@@ -5,41 +5,10 @@ import java.util.List;
 
 public class Account extends AbstractAccount {
 	
-	List<ITransaction> transactions = new ArrayList<>();
-	String accountNumber;
-	double currentBalance;
-	
 	public Account(ICustomer owner, String accountNumber) {
 		addOwner(owner);
 		this.accountNumber = accountNumber;
 		super.interest = 0.01;
-	}
-	
-	@Override
-	public String accountNumber() {
-		return accountNumber;
-	}
-	
-	@Override
-	public void addTransaction(ITransaction transaction) {
-		transactions.add(transaction);
-		currentBalance += transaction.amount();
-	}
-
-	@Override
-	public double getBalance() {
-		return currentBalance;
-	}
-
-	@Override
-	public String toString() {
-		return "Account = accountNumber: " + accountNumber + ", currentBalance: " + currentBalance;
-	}
-
-	@Override
-	public void addInterest() {
-	 this.currentBalance += this.currentBalance * this.getInterest();
-		
 	}
 
 }
