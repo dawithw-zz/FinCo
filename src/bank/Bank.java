56d71f4ebc513bb42ea3339a7df641b65715fb72
Dawit Woldegiorgis <dwoldegiorgis@mum.edu>
2018-10-24 00:03:54 -0500

    added bank logic

--- src/bank/Bank.java
+++ src/bank/Bank.java
@@ -1,7 +1,24 @@
 package bank;
 
-import controller.FinCo;
+import controller.*;
+import view.*;
 
 public class Bank extends FinCo{
-
+	
+	public static void createPersonalCheckingAccount(AccountParameters param) {
+		createCustomerAccount(param, PersonFactory.getInstance(), CheckingAccountFactory.getInstance());
+	}
+	
+	public static void createPersonalSavingAccount(AccountParameters param) {
+		createCustomerAccount(param, PersonFactory.getInstance(), SavingAccountFactory.getInstance());
+	}
+	
+	public static void createCompanyCheckingAccount(AccountParameters param) {
+		createCustomerAccount(param, CompanyFactory.getInstance(), CheckingAccountFactory.getInstance());
+	}
+	
+	public static void createCompanySavingAccount(AccountParameters param) {
+		createCustomerAccount(param, CompanyFactory.getInstance(), SavingAccountFactory.getInstance());
+	}
+	
 }
